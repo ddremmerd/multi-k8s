552fe60006c7f6c7d00b81af43ab147d5422c371
Kuntika <suwanannn.k@gmail.com>
2022-01-26 21:54:14 +0700

    added deployment sctipt

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t testecx14/multi-client:latest -t testecx14/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t testecx14/multi-server:latest -t testecx14/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t testecx14/multi-worker:latest -t testecx14/multi-worker:$SHA -f ./worker/Dockerfile ./worker 
+docker push testecx14/multi-client:latest
+docker push testecx14/multi-server:latest
+docker push testecx14/multi-worker:latest
+
+docker push testecx14/multi-client:$SHA
+docker push testecx14/multi-server:$SHA
+docker push testecx14/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=testecx14/multi-server:$SHA
+kubectl set image deployments/client-deployment client=testecx14/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=testecx14/multi-worker:$SHA
+
