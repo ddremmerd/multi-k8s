4622aa45d3d02b0c8daebaa12335544c8149b4ef
Kuntika <suwanannn.k@gmail.com>
2022-02-07 14:57:32 +0700

    edit file

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,5 @@ docker push testecx14/multi-worker:$SHA
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=testecx14/multi-server:$SHA
 kubectl set image deployments/client-deployment client=testecx14/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=testecx14/multi-worker:$SHA
+kubectl set image deployments/worker-deployment worker=testecx14/multi-worker:$SHA
 
